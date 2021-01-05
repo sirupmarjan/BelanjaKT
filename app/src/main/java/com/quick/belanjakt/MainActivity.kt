9557a215946684f957898bc08e0bc86a9accf526
Violin <rizki_violin_radhiyan@git.com>
2021-01-05 13:08:10 +0700

    successfully display image from firebase storage

--- app/src/main/java/com/quick/belanjakt/MainActivity.kt
+++ app/src/main/java/com/quick/belanjakt/MainActivity.kt
@@ -3,10 +3,16 @@ package com.quick.belanjakt
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.tasks.Task
+import com.bumptech.glide.Glide
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.storage.FileDownloadTask
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
 
@@ -14,7 +20,7 @@ import java.io.File
 private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
-    lateinit var mStorageReference : StorageReference
+    lateinit var mStorageReference: StorageReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -28,9 +34,30 @@ class MainActivity : AppCompatActivity() {
 
             startActivityForResult(Intent.createChooser(intent, "Select a file"), 111)
         }
+
+        btn_download.setOnClickListener {
+            downloadProcedure()
+        }
     }
 
-    private fun uploadProcedure(dataSelected : Uri) {
+    private fun downloadProcedure() {
+        val localFile = File.createTempFile("images", ".jpg")
+        val riversRef: StorageReference = mStorageReference.child("data/sun eater.jpg")
+        val imgView: ImageView = findViewById(R.id.iv_sirup)
+
+        riversRef.downloadUrl.addOnSuccessListener { Uri ->
+            var strImage : String = Uri.toString()
+            Glide.with(this)
+                .load(strImage)
+                .into(imgView)
+
+        }
+
+
+
+    }
+
+    private fun uploadProcedure(dataSelected: Uri) {
 
 //        val file: Uri = Uri.fromFile(File("path/to/images/rivers.jpg"))
         val file: Uri = dataSelected
