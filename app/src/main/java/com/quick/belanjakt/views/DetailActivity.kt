bbbddc8e8fabf642a81c7d19d6f38778f45afd38
Violin <rizki_violin_radhiyan@git.com>
2021-01-15 14:23:04 +0700

    fix the bug

--- app/src/main/java/com/quick/belanjakt/views/DetailActivity.kt
+++ app/src/main/java/com/quick/belanjakt/views/DetailActivity.kt
@@ -1,24 +1,22 @@
 package com.quick.belanjakt.views
 
-import android.content.Intent
+import android.app.Activity
+import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.graphics.Paint
-import android.net.Uri
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import com.quick.belanjakt.databinding.ActivityDetailBinding
 import com.quick.belanjakt.models.ContentOfflineDatabase
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import com.squareup.picasso.Picasso
+import kotlinx.coroutines.*
 import java.text.NumberFormat
 import java.util.*
+
 
 class DetailActivity : AppCompatActivity() {
     lateinit var binding: ActivityDetailBinding
@@ -43,9 +41,6 @@ class DetailActivity : AppCompatActivity() {
     private fun getItemRun(
         documentID: String,
     ) = CoroutineScope(Dispatchers.IO).launch {
-        val db: SQLiteDatabase = kontenHelper.getWritableDatabase()
-        val mQuery2 = "SELECT * FROM tb_konten where documentID = '$documentID'"
-        val c = db.rawQuery(mQuery2, null)
         lateinit var nNamaKonten: String
         lateinit var nDeskripsi: String
         lateinit var nImageUrl: String
@@ -53,7 +48,10 @@ class DetailActivity : AppCompatActivity() {
         var nHargaAwal: Int = 0
         var nRating: Int = 0
         var nFreeOngkir: Int = 0
-        while (c.moveToFirst()) {
+        val db: SQLiteDatabase = kontenHelper.getWritableDatabase()
+        val mQuery2 = "SELECT * FROM tb_konten where documentID = '$documentID'"
+        val c = db.rawQuery(mQuery2, null)
+        while (c.moveToNext()) {
             nNamaKonten = c.getString(c.getColumnIndex("judul"))
             nHarga = c.getInt(c.getColumnIndex("harga"))
             nHargaAwal = c.getInt(c.getColumnIndex("harga_awal"))
@@ -61,49 +59,84 @@ class DetailActivity : AppCompatActivity() {
             nFreeOngkir = c.getInt(c.getColumnIndex("free_shipping"))
             nRating = c.getInt(c.getColumnIndex("rating"))
             nImageUrl = c.getString(c.getColumnIndex("image_url"))
-            withContext(Dispatchers.Main){
-                setView(nNamaKonten, nHarga,nHargaAwal, nDeskripsi, nFreeOngkir,nRating,nImageUrl)
+            withContext(Dispatchers.Main) {
+                setView(
+                    nNamaKonten,
+                    nHarga,
+                    nHargaAwal,
+                    nDeskripsi,
+                    nFreeOngkir,
+                    nRating,
+                    nImageUrl
+                )
             }
         }
     }
 
-    private fun setView(nNamaKonten: String?, nHarga: Int, nHargaAwal: Int, nDeskripsi: String?, nFreeOngkir: Int, nRating: Int, nImageUrl: String?) {
+    private fun setView(
+        nNamaKonten: String?,
+        nHarga: Int,
+        nHargaAwal: Int,
+        nDeskripsi: String?,
+        nFreeOngkir: Int,
+        nRating: Int,
+        nImageUrl: String?
+    ) = CoroutineScope(Dispatchers.Main).launch {
         storageReference = FirebaseStorage.getInstance().getReference()
         val riversRef: StorageReference = storageReference.child(nImageUrl!!)
         riversRef.downloadUrl.addOnSuccessListener { Uri ->
-
-            if (this != null){
-
-                Glide.with(this)
-                    .load(Uri.toString())
-                    .centerCrop()
-                    .into(binding.ivProduk)
-            }
+//            if (isValidContextForGlide(this)){
+//                Glide.with(this)
+//                    .load(Uri.toString())
+//                    .centerCrop()
+//                    .into(binding.ivProduk)
+//            }
+            Glide.with(this@DetailActivity)
+                .load(Uri.toString())
+                .centerCrop()
+                .into(binding.ivProduk)
+//            Picasso.get()
+//                .load(Uri.toString())
+//                .resize(50, 50)
+//                .centerCrop()
+//                .into(binding.ivProduk)
         }
 
 
         binding.tvNamaKonten.text = nNamaKonten
         binding.tvDescriptionKonten.text = nDeskripsi
         binding.tvHargaKonten.text = formatRupiah.format(nHarga)
-        if (nHargaAwal != 0){
-            var disc : Long= nHargaAwal.toLong()-nHarga.toLong()
-            var percenDiscount :Long = disc*100 / nHargaAwal.toLong()
+        if (nHargaAwal != 0) {
+            var disc: Long = nHargaAwal.toLong() - nHarga.toLong()
+            var percenDiscount: Long = disc * 100 / nHargaAwal.toLong()
             binding.tvDiskonKonten.apply {
                 paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
                 text = formatRupiah.format(nHargaAwal)
             }
             binding.tvPersenDiskon.text = "-$percenDiscount%"
-        }else{
+        } else {
             binding.tvDiskonKonten.visibility = View.GONE
             binding.tvPersenDiskon.visibility = View.GONE
         }
-        if (nFreeOngkir == 0){
+        if (nFreeOngkir == 0) {
             binding.ivFreeOngkir.visibility = View.GONE
         }
         binding.ratingBarKonten.rating = nRating.toFloat()
 
     }
 
+    fun isValidContextForGlide(context: Context?): Boolean {
+        if (context == null) {
+            return false
+        }
+        if (context is Activity) {
+            val activity = context as Activity
+            if (activity.isDestroyed || activity.isFinishing) {
+                return false
+            }
+        }
+        return true
+    }
 //    override fun onBackPressed() {
 //        Toast.makeText(this, " back pressed", Toast.LENGTH_SHORT).show()
 //    }
